5be9fe1203ba3c7f79cd0bf9d786d6d13cfb5930
kamalnath69 <kamalnath9669@gmail.com>
2024-05-20 16:14:14 +0530

    need change in dependencies

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -7,34 +7,36 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SessionProvider } from './App/Screen/HomeScreen/SessionContext';
 import AppNavigator from './App/Navigations/AppNavigator';
 
-
 const tokenCache = {
   async getToken(key) {
     try {
-      return SecureStore.getItemAsync(key);
+      const token = await SecureStore.getItemAsync(key);
+      console.log('Retrieved token:', token); // Log retrieved token
+      return token;
     } catch (err) {
+      console.error('Error retrieving token:', err); // Log retrieval error
       return null;
     }
   },
   async saveToken(key, value) {
     try {
-      return SecureStore.setItemAsync(key, value);
+      console.log('Saving token:', value); // Log token being saved
+      await SecureStore.setItemAsync(key, value);
     } catch (err) {
-      return;
+      console.error('Error saving token:', err); // Log saving error
     }
   },
 };
 
 export default function App() {
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey='pk_test_c2FjcmVkLWVsZXBoYW50LTIxLmNsZXJrLmFjY291bnRzLmRldiQ'>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={'pk_test_c2FjcmVkLWVsZXBoYW50LTIxLmNsZXJrLmFjY291bnRzLmRldiQ'}>
       <SessionProvider>
         <NavigationContainer>
           <View style={styles.container}>
             <SignedIn>
               <AppNavigator/>
             </SignedIn>
-            
             <SignedOut>
               <Login/>
             </SignedOut>
